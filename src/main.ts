85b8c2fbe4e46846f4df3eb5fae86f04f1e54661
Arnoldus Bryan Hendry <bryanhendry12@gmail.com>
2025-12-21 22:46:52 +0700

    backend initialization

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api/v1'); // 👈 THIS LINE
+
+  await app.listen(3000);
+}
+bootstrap();
